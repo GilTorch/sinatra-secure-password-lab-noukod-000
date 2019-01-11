0692b75d9df3df596d513f6b98fcbdf75b3ad644
Gilbert Torchon <gilbert@noukod.com>
2019-01-11 19:57:39 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,7 @@ class ApplicationController < Sinatra::Base
     #your code here
     puts "Trying to sign up"
     @user=User.create(username:@params[:username],password:@params[:password])
+    @user.username
     binding.pry
   end
 
